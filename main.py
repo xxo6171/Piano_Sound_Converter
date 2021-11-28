db67ab4eae79ad6d80dabc835ff08a4198162aef
kyungmin <kyung971112@gmail.com>
2021-11-28 21:53:10 +0900

    절대 pitch값 변환

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from convert_16kHz_mono import convert_16kHz_mono
 from tensorflow_hub_model import tensorflow_hub_model
-
+from output_hz import output_hz
 # from spectrogram import spectrogram_plot_stft
 # from time_domain import timeDomain_plot
 # from frequency_domain import freDomain_plot
@@ -18,7 +18,13 @@ def PSC(data) :
     '''2. SPICE 모델을 사용해 추정 pitch값 반환'''
     indices, pitch_outputs, confidence_outputs = tensorflow_hub_model(converted_mono_audio)
 
+    '''3. 반환 받은 pitch값을 절대 pitch값으로 변환, confidence 값이 0.9보다 크면 절대 피치 값 배열에 저장 그 외 0 저장'''
+    confident_pitch_values_hz = [output_hz(p) if c >= 0.9 else 0 for i, p, c in zip(indices, pitch_outputs, confidence_outputs)]
+    # print(confident_pitch_values_hz)
 PSC('Audios/input_audio.wav')
+
+
+
 
 '''데이터 시각화, PSC 메소드 안에 넣어서 사용'''
 #timeDomain_plot(16000, converted_mono_audio)
---
+++ output_hz.py
@@ -0,0 +1,9 @@
+
+'''SPICE가 반환하는 pitch 값은 0에서 1사이의 범위에 있기에 절대 피치 값(hz)로 변환'''
+def output_hz(pitch_output):
+  PT_OFFSET = 25.58
+  PT_SLOPE = 63.07
+  FMIN = 10.0;
+  BINS_PER_OCTAVE = 12.0;
+  cqt_bin = pitch_output * PT_SLOPE + PT_OFFSET;
+  return FMIN * 2.0 ** (1.0 * cqt_bin / BINS_PER_OCTAVE)
