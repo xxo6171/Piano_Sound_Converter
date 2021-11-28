081ddccc70c5ae848b8bba03949495f2709fe3ff
kyungmin <kyung971112@gmail.com>
2021-11-28 21:38:13 +0900

    스펙트로그램 그래프 모듈 추가

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from convert_16kHz_mono import convert_16kHz_mono
+from spectrogram import spectrogram_plot_stft
 from time_domain import timeDomain_plot
 from frequency_domin import freDomain_plot
 
 def PSC(data) :
     convert_mono_output, converted_mono_audio = convert_16kHz_mono(data)
-    timeDomain_plot(16000, converted_mono_audio)
+    # timeDomain_plot(16000, converted_mono_audio)
+    # freDomain_plot(converted_mono_audio)
+    spectrogram_plot_stft(converted_mono_audio / 32768.0, sample_rate=16000)
 PSC('Audios/input_audio.wav')
---
+++ spectrogram.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa
+from librosa import display as librosadisplay
+
+'''스펙트로그램 그래프'''
+'''오디오 샘플 int16'''
+MAX_ABS_INT16 = 32768.0
+
+def spectrogram_plot_stft(x, sample_rate, show_black_and_white=False):
+  x_stft = np.abs(librosa.stft(x, n_fft=2048))
+  fig, ax = plt.subplots()
+  fig.set_size_inches(10, 5)
+  x_stft_db = librosa.amplitude_to_db(x_stft, ref=np.max)
+  if(show_black_and_white):
+    librosadisplay.specshow(data=x_stft_db, y_axis='log',
+                             sr=sample_rate, cmap='gray_r')
+  else:
+    librosadisplay.specshow(data=x_stft_db, y_axis='log', sr=sample_rate)
+
+  plt.xlabel('Time')
+  plt.ylabel('Frequency')
+  plt.colorbar(format='%+2.0f dB')
+  plt.show()
+
+
