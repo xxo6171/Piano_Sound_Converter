c225b45fbfcc9ec88c3ad8d548c128095dca14e0
Parksangwoo <jinloveme11@naver.com>
2021-11-28 21:25:41 +0900

    시간영역그래프 모듈추가

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from convert_16kHz_mono import convert_16kHz_mono
+from time_domain import timeDomain_plot
 
 def PSC(data) :
     convert_mono_output, converted_mono_audio = convert_16kHz_mono(data)
-
+    timeDomain_plot(16000, converted_mono_audio)
 PSC('Audios/input_audio.wav')
---
+++ time_domain.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#시간 영역 그래프
+def timeDomain_plot(x, data) :
+    x = np.linspace(0, len(data)/16000, len(data))
+    plt.plot(x, data)
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    plt.title("Time domain")
+    plt.show()
