306e0ed4f81baa54589fe408d92972f1206dfdb8
kyungmin <kyung971112@gmail.com>
2021-12-07 20:40:39 +0900

    lpf 주석 추가

--- Modules/low_pass_filter.py
+++ Modules/low_pass_filter.py
@@ -1,5 +1,5 @@
 from scipy import signal
-
+'''low pass filter 함수입니다'''
 def lowpassfilter(data) :
     b = signal.firwin(101, cutoff=1000, fs=16000, pass_zero='lowpass')
     fil_data  = signal.lfilter(b, [1.0], data)
