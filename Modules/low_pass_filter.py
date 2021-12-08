7479431cbfa79c814bf27c6c2c6704a061206782
kyungmin <kyung971112@gmail.com>
2021-12-08 23:07:21 +0900

    lpf 수정

--- Modules/low_pass_filter.py
+++ Modules/low_pass_filter.py
@@ -3,5 +3,4 @@ from scipy import signal
 def lowpassfilter(data) :
     b = signal.firwin(101, cutoff=1000, fs=16000, pass_zero='lowpass')
     fil_data  = signal.lfilter(b, [1.0], data)
-
     return fil_data
